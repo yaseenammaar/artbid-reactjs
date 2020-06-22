131443feefba8ad8f21c34db495063742a37f64b
Shubham singh <46085085+proshubham5@users.noreply.github.com>
2020-06-22 19:18:35 +0530

    added CORS support in apis; fixed bugs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,8 @@ import * as admin from 'firebase-admin';
 import * as express from 'express';
 import * as bodyParser from "body-parser";
 
+import * as cors from 'cors'
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
@@ -13,34 +15,36 @@ admin.initializeApp(functions.config().firebase);
 const app = express();
 const main = express();
 
+app.use(cors({ origin: true }));
+main.use(cors({ origin: true }));
 main.use('/api/v1', app);
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: false }));
 
-const db = admin.database();
+const db = admin.database()
 
 app.post('/checkLatestBidder', async (req, res) => {
     try {
         const itemKey = req.body['itemKey']
         const uid = req.body['uid']
 
-        const ref = db.ref("items/" + itemKey + "/bidders/")
+        const ref = db.ref("items/" + itemKey + "/bidders")
         const query = ref.orderByKey().limitToLast(1)
 
         await query.once("value",successCallback)
 
-        // @ts-ignore
-        function successCallback(snap) {
-            const got_uid = snap.val().userid
+        function successCallback(snap:any) {
+
+            const got_uid = snap.child('userid').val()
             if(uid != got_uid) {
                 //response is yes;
                 //update the database
-                res.send("he or she becomes latest bidder")
+                res.status(200).send("he or she becomes latest bidder")
             }
             else {
                 //response is no
                 //don't update database
-                res.send("he or she is already latest bidder")
+                res.status(200).send("he or she is already latest bidder")
             }
         }
 
@@ -49,6 +53,5 @@ app.post('/checkLatestBidder', async (req, res) => {
         res.status(400).send(`Sorry! Some error came at our side. Please try again`)
     }
 })
-
 
 export const webApi = functions.https.onRequest(main);
