9f2a6d796a3b63e1dfe7c41e5930a2d070e38813
yaseenammaar <yammaar9@gmail.com>
2021-04-11 12:11:20 +0530

    PrePull

--- src/logics/uploadItems.js
+++ src/logics/uploadItems.js
@@ -2,32 +2,28 @@ import apiRequest from "./apiRequest";
 import APIS from "../constants/Apis";
 import firebaseInstance from "../Firebase/index";
 
-const getSearchSuggestions = async (searchText: string) => {
+const uploadItems = async (uploadData) => {
 
     try {
 
         const requestData = {
-        searchText
+            available_state: uploadData.available_state,
+            base_price: uploadData.base_price,
+            by_user: uploadData.by_user,
+            caption: uploadData.caption,
+            category: uploadData.category,
+            closing_date: uploadData.closing_date,
+            closing_time: uploadData.closing_time,
+            status: uploadData.status,
+            title: uploadData.title,
+            featured_image: uploadData.featured_image,
+            supporting_images: uploadData.supporting_images,
         };
 
-        // const res = await apiRequest('post', APIS.PROTECTED_APIS.GET_SEARCH_SUGGESTIONS, requestData, 'application/json', true);
-        // return res.data.suggestions
+        const res = await apiRequest('post', APIS.PROTECTED_APIS.ITEM_UPLOAD, requestData, 'application/json', true);
+        return res.data.suggestions
 
-        const db = firebaseInstance.firestore()
-        const itemCollection = db.collection('keywords')
-
-        const snapshots = await itemCollection
-            .where("key_map", 'array-contains', searchText)
-            .orderBy("score", 'desc')
-            .limit(10)
-            .get()
-
-        let suggestions = []
-        snapshots.forEach(snap => {
-            suggestions.push(snap.id)
-        })
-
-        return suggestions
+      
     } catch (e) {
         console.log(e.message)
     }
