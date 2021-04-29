cb2544dd7fdd59d9d2d3b8df6ed1dedaeeaa7aab
yaseenammaar <yammaar9@gmail.com>
2021-04-30 01:00:24 +0530

    GIF Support

--- src/components/molecules/Hero.js
+++ src/components/molecules/Hero.js
@@ -278,23 +278,23 @@ function Hero() {
 
             <section className="card-collection">
                 <div className="d-flex justify-content-between align-items-center collection-title container">
-                    <h2 className="m-0"> SKETCHES </h2>
+                    <h2 className="m-0"> Exclusive </h2>
                     <a className="m-0" href="#">See All</a>
                 </div>
 
                 <div className="container">
                     <div className="row mt-5">
                     <div className="col-md-3 col-6">
-                            <OneCard url="https://cdn.thewirecutter.com/wp-content/uploads/2020/04/laptops-lowres-2x1--1024x512.jpg" name="Apple macbook and ipad" category="Gadgets" artist="John" bp="14,777"/>
+                            <OneCard url="https://i.pinimg.com/originals/76/ad/64/76ad64dc948e54d09e46f93320be77af.gif" name="Apple macbook and ipad" category="Gadgets" artist="John" bp="14,777"/>
                             </div>
                             <div className="col-md-3 col-6">
-                                <OneCard url="https://images.barrons.com/im-317412?width=620&size=1.5005861664712778" name="USA cents rare coin" category="Coins" artist="Omar" bp="100"/>
+                                <OneCard url="https://i.pinimg.com/originals/68/0b/eb/680beb29a683b6624393df56ac23e9bf.gif" name="USA cents rare coin" category="Coins" artist="Omar" bp="100"/>
                             </div>
                             <div className="col-md-3 col-6">
-                                <OneCard url="https://akm-img-a-in.tosshub.com/sites/btmt/images/stories/goldmedal_660_083118065009.jpg"  name="Gold Medal" category="Medal & Trophy" artist="Usain Bolt" bp="34,800"/>
+                                <OneCard url="https://i.pinimg.com/originals/1a/37/bc/1a37bcf72db69c5ee822868209e18b76.gif"  name="Gold Medal" category="Medal & Trophy" artist="Usain Bolt" bp="34,800"/>
                             </div>
                             <div className="col-md-3 col-6">
-                                <OneCard url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFWsmx_wbVSd_akyRiShZWnoem2Ywlc7-9VA&usqp=CAU" name="Very Moment Robbery Footage" category="News" artist="Nightcrawler" bp="5,000"/>
+                                <OneCard url="https://i.pinimg.com/originals/1c/88/83/1c8883a1768f2f77caf0371d49a68dc2.gif" name="NASA Rocket" category="Astronomy" artist="Nightcrawler" bp="5,000"/>
                             </div>
                             
                     </div>
